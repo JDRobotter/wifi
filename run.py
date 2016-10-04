3f32b38ff26331656795f520369e1df1d02fd0f7
Mehdi <mlauters@fly-n-sense.com>
2016-10-04 09:48:17 +0000

    FTP handler init

--- run.py
+++ run.py
@@ -217,8 +217,46 @@ class Karma2:
       log("[%s] POP3 server on port %d is shutting down"%(_ctxt('x',RED),self.port))
 
   class FTPTCPRequestHandler(BaseRequestHandler):
+    
+    def respond(self, code, explanation):
+      """Send a response to the client."""
+      self.request.send('%d %s\r\n' % (code, explanation))
+    
+    def process_request(self):
+      """Parse input into a command and an argument."""
+      data = self.request.recv(64)
+      if data == "":
+        return None
+      parts = data.strip().split(' ')
+      return parts.pop(0), parts
+    
     def handle(self):
-      log("[%s] %s"%(_ctxt('!',RED),_ctxt('Connexion received, yet the fake FTP server is not implemented',RED)))
+      client = self.client_address[0]
+      self.respond(220, "FTP server")
+      user = ''
+      password = ''
+      while True:
+        req = self.process_request()
+        if req is None:
+          break
+        cmd, args = req
+        if cmd == 'USER':
+          user = (args and args[0] or '*missing*')
+          self.respond(331, 'Please specify the password.')
+        elif cmd == 'PASS':
+          password = (args and args[0] or '*missing*')
+          log("ftp login from %s user: %s, password: %s"%(client, _ctxt(user,RED), _ctxt(password,RED)))
+          self.respond(230, 'Valid user.')
+        elif cmd == 'PWD':
+          self.respond(212, '/')
+        elif cmd == 'TYPE':
+          self.respond(215, 'UNIX')
+        elif cmd == 'PASV':
+          self.respond(227, 'Passive mode')
+        else:
+          log("%s %s refused, login required"%(cmd, ' '.join(args)))
+          self.respond(530, 'Please login with USER and PASS.')
+
 
   class FTPTCPServer(ThreadingMixIn, TCPServer):
     allow_reuse_address = True
