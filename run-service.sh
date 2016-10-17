50334f2f32386ac727ea1a00c2c584b3f7cee1cb
JD <jdbrossillon@fly-n-sense.com>
2016-10-17 15:19:09 +0200

    fixing service

--- run-service.sh
+++ run-service.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-
 d=$(dirname $0)
-$d/run.py -o -n "orange" -n "SFR WiFi FON" -n "FreeWifi" -n "Bouygues Telecom Wi-Fi" -n "SFR WiFi Mobile" -a wlan0,wlan1,wlan2,wlan3,wlan4
+cd $d
+
+systemctl stop dnsmasq
+
+sleep 10
+
+./run.py -o -n "orange" -n "SFR WiFi FON" -n "FreeWifi" -n "Bouygues Telecom Wi-Fi" -n "SFR WiFi Mobile" -a wlan0,wlan1,wlan2,wlan3,wlan4
